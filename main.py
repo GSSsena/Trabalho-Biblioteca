5f67733e96abf08b4a0dee47a02aaefe65faf1e3
Guilherme Sema <guilhermedasilvasena960@gmail.com>
2025-10-03 11:04:55 -0300

    Criando banco de dados

---
+++ main.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conexao = sqlite3.connect("Biblioteca.db")
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS livros(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,         
+    ano  INTEGER,
+    disponivel TEXT            
+)
+""")
